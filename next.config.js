58f8be457ae79ebff24aee239b1e36e3646f18ef
spin~ <spin@seized.email>
2022-08-14 23:39:25 -0700

    hi

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
+    unoptimized: true,
     domains: ['cdn.spin.rip', 'cdn.discordapp.com', 'i.scdn.co']
   },
   basePath: '/my-site'
