72d2923dd23151922239863481bebdf4ed81112a
spin~ <65109978+spinfal@users.noreply.github.com>
2022-10-03 23:30:33 +0000

    gogocdn.net image proxy

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['cdn.spin.rip', 'cdn.discordapp.com', 'i.scdn.co']
+    domains: ['cdn.spin.rip', 'cdn.discordapp.com', 'i.scdn.co', 'gogocdn.net']
   },
 };
 
